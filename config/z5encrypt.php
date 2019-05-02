d259170478e7684c40d6344de52f7db99105123c
William Zhou <2693558149@qq.com>
2019-05-02 12:55:18 +0000

    Apply fixes from StyleCI

--- config/z5encrypt.php
+++ config/z5encrypt.php
@@ -4,7 +4,7 @@ return [
     // How to get secret and token?
     // https://z5encrypt.com/encrypt/api
 
-    'secret' => env('Z5ENCRYPT_SECRET'),
-    'token' => env('Z5ENCRYPT_TOKEN'),
-    'api_version' => '0.1'
+    'secret'      => env('Z5ENCRYPT_SECRET'),
+    'token'       => env('Z5ENCRYPT_TOKEN'),
+    'api_version' => '0.1',
 ];
